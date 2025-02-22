b6a0fb1f65fd888667fce24c69e6c9cd66828cb6
Monu Nigaam <monunigaam1658@gamil.com>
2025-02-23 00:10:49 +0530

    Added Home Page layout and styling

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Player from "./pages/Player/Player";
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/player" element={<Player />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Login.css'
+
+const Login = () => {
+  return (
+    <div className='login'>
+      ll;asdfojf login page
+    </div>
+  )
+}
+
+export default Login
